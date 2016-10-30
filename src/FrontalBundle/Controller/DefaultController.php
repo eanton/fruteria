56724749d3c37555d61054ee6c14d9a075033155
eanton <eanton@gmail.com>
2016-10-30 15:54:15 +0100

    Ejemplo como usar un TEMPLATE para una WEB

--- src/FrontalBundle/Controller/DefaultController.php
+++ src/FrontalBundle/Controller/DefaultController.php
@@ -8,11 +8,16 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontalBundle:Default:index.html.twig');
+        return $this->render('FrontalBundle:Default:nuevoIndex.html.twig');
     }
 	
 	public function mipaginainicialAction()
     {
         return $this->render('FrontalBundle:Default:mipaginainicial_template.html.twig');
+    }
+	
+		public function otranuevapaginaAction()
+    {
+        return $this->render('FrontalBundle:Default:otrapagina.html.twig');
     }
 }
